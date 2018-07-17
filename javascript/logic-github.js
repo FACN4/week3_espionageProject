bfb5d9481127252d97527d6277d360e7f1c555d2
tawfiknasser <tawfiknasser@gmail.com>
2018-07-17 16:39:21 +0300

    #7 added filter function shell

---
+++ javascript/logic-github.js
@@ -0,0 +1,15 @@
+var doFunctions = {
+  filterGitHub: (function(obj) {
+
+    // depends on the obj template
+
+
+    return obj;
+
+  })()
+};
+
+
+if (typeof module !== "undefined") {
+  module.exports = doFunctions;       // export to the tests
+}
