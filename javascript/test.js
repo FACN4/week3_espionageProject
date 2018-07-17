be9c17979c6517a8d28edc9424cc7eb9453eaa8e
tawfiknasser <tawfiknasser@gmail.com>
2018-07-18 00:14:25 +0300

    #13  & #16 nyc now work + added the test script

---
+++ javascript/test.js
@@ -0,0 +1,6 @@
+const tape = require('tape');
+const doFunctions = require('./logic-github.js');
+tape("addTodo Function logic work", function(t) {
+  t.equal(1,1,"Test Is working");
+  t.end();
+});
